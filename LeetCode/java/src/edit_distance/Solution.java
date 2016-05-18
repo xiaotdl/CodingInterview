c6a77806ca5990811169c8ce5d4ea66bcc9e5052
xiaotdl <xiaotdl@gmail.com>
2016-05-18 15:43:23 -0700

    init

---
+++ LeetCode/java/src/edit_distance/Solution.java
@@ -0,0 +1,46 @@
+package edit_distance;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by xili on 5/18/16.
+ */
+public class Solution {
+    // tag: string, dp
+    // time: O(m + n), one pass through each string
+    // space: O(mn), used two dimensional matrix to save path
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word2 == null) {
+            return -1;
+        }
+
+        // dp[i][j]: minDistance(word1[:i], word2[:j])
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        // when word1 is empty
+        for (int j = 0; j <= word2.length(); j++) {
+            dp[0][j] = j;
+        }
+        // when word2 is empty
+        for (int i = 1; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int replace = dp[i - 1][j - 1] + 1;
+                    int insert = dp[i - 1][j] + 1;
+                    int delete = dp[i][j - 1] + 1;
+                    dp[i][j] = Collections.min(Arrays.asList(replace, insert, delete));
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+
+}
---
+++ LeetCode/java/src/group_anagrams/Solution.java
@@ -0,0 +1,41 @@
+package group_anagrams;
+
+import java.util.*;
+
+/**
+ * Created by xili on 5/18/16.
+ */
+public class Solution {
+    // tag: string, hash table
+    // time: O(n*m*logm), where n is length of strs, m is average length of strings
+    // space: O(n), used a hash to save all strings
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+
+        // key is sorted String, where anagrams will have the same sorted String.
+        HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+
+        for (String str: strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!hm.containsKey(sortedStr)) {
+                ArrayList<String> anagrams = new ArrayList<String>();
+                hm.put(sortedStr, anagrams);
+            }
+            hm.get(sortedStr).add(str);
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry: hm.entrySet()){
+            ArrayList<String> anagrams = entry.getValue();
+            Collections.sort(anagrams);
+            result.add(anagrams);
+        }
+
+        return result;
+    }
+}

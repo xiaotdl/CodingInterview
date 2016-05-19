c0492c9911669f6e73c9d0effe7655ef4545b0be
xiaotdl <xiaotdl@gmail.com>
2016-05-18 17:20:31 -0700

    init

---
+++ LeetCode/java/src/add_binary/Solution.java
@@ -0,0 +1,37 @@
+package add_binary;
+
+/**
+ * Created by xili on 5/18/16.
+ */
+public class Solution {
+    // tag: string, two pointer
+    // time: O(m + n), one pass through each string
+    // space: O(1), no additional space used
+    public String addBinary(String a, String b) {
+        if (a == null || b == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            Character charA = (i >= 0) ? a.charAt(i) : '0';
+            Character charB = (j >= 0) ? b.charAt(j) : '0';
+            int sum = (int)(charA-'0') + (int)(charB-'0') + carry;
+            sb.insert(0, sum % 2);
+            carry = sum / 2;
+            i--;
+            j--;
+        }
+
+        if (carry == 1) {
+            sb.insert(0, 1);
+        }
+
+        return sb.toString();
+    }
+}

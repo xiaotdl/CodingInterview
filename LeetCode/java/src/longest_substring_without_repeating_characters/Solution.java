1f06bb21a431941ee7a43dc73bed9ea0ecbb1697
xiaotdl <xiaotdl@gmail.com>
2017-06-30 21:27:23 -0700

    update

--- LeetCode/java/src/longest_substring_without_repeating_characters/Solution.java
+++ LeetCode/java/src/longest_substring_without_repeating_characters/Solution.java
@@ -12,17 +12,17 @@ public class Solution {
     // space: O(1)
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) return 0;
-        if (s.length() == 1) return 1;
-        int l = 0;
-        int r = 0;
+
         char[] S = s.toCharArray();
         Set<Character> set = new HashSet<>();
+        int l = 0;
+        int r = 0;
         int max = 0;
         while (r < s.length()) {
             if (!set.contains(S[r])) {
                 set.add(S[r]);
-                r++;
                 max = Math.max(max, set.size());
+                r++;
             }
             else {
                 set.remove(S[l]);

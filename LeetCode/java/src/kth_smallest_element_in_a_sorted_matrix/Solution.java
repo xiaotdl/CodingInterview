4307a9f738c9b87e31a374493734fee4b550f270
xiaotdl <xiaotdl@gmail.com>
2017-09-16 01:28:16 -0700

    update

--- LeetCode/java/src/kth_smallest_element_in_a_sorted_matrix/Solution.java
+++ LeetCode/java/src/kth_smallest_element_in_a_sorted_matrix/Solution.java
@@ -11,48 +11,48 @@ public class Solution {
     // time: O(klogk)
     // space: O(k)
     class Element {
-        int i;
-        int j;
-        int val;
-        Element(int i, int j, int val) {
-            this.i = i;
-            this.j = j;
+        int x, y, val;
+        Element(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
             this.val = val;
         }
     }
+
+    /*
+     * @param matrix: a matrix of integers
+     * @param k: An integer
+     * @return: the kth smallest number in the matrix
+     */
     public int kthSmallest(int[][] matrix, int k) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return -1;
-        }
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        // min heap
-        PriorityQueue<Element> pq = new PriorityQueue<>(k,
+        PriorityQueue<Element> minHeap = new PriorityQueue<>(k,
                 new Comparator<Element>(){
                     @Override
-                    public int compare(Element a, Element b) {
-                        return a.val - b.val;
+                    public int compare(Element e1, Element e2) {
+                        return e1.val - e2.val;
                     }
                 }
         );
 
-        pq.offer(new Element(0, 0, matrix[0][0]));
-        int result = -1;
-        while (!pq.isEmpty() && k-- > 0) {
-            Element currElement = pq.poll();
-            result = currElement.val;
-            int i = currElement.i;
-            int j = currElement.j;
-            if (j + 1 < cols) {
-                pq.offer(new Element(i, j + 1, matrix[i][j + 1]));
+        minHeap.offer(new Element(0, 0, matrix[0][0]));
+        // take out k-1 elments
+        while (!minHeap.isEmpty() && k - 1 > 0) {
+            Element curr = minHeap.poll();
+            int x = curr.x;
+            int y = curr.y;
+            if (y < n - 1) {
+                minHeap.offer(new Element(x, y + 1, matrix[x][y + 1]));
             }
-            if (j == 0 && i + 1 < rows) {
-                pq.offer(new Element(i + 1, j, matrix[i + 1][j]));
+            if (y == 0 && x < m - 1) {
+                minHeap.offer(new Element(x + 1, y, matrix[x + 1][y]));
             }
+            k--;
         }
-
-        return result;
+        return minHeap.peek().val;
     }
 }

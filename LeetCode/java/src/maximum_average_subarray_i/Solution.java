7c05cb97a2fc98b667d7be853778450388c2d69d
xiaotdl <xiaotdl@gmail.com>
2017-09-25 03:07:03 -0700

    update

--- LeetCode/java/src/maximum_average_subarray_i/Solution.java
+++ LeetCode/java/src/maximum_average_subarray_i/Solution.java
@@ -12,35 +12,14 @@ public class Solution {
         if (nums == null || nums.length < k || k < 0 || k > nums.length) return 0;
 
         int sum = 0;
-        int l = 0;
-        int r = k - 1;
-        for (int i = l; i <= r; i++) {
-            sum += nums[i];
-        }
-        int max = sum;
-        while (r < nums.length - 1) {
-            r++;
-            sum = sum - nums[l] + nums[r];
-            max = Math.max(max, sum);
-            l++;
-        }
-        return (double) max / k;
-    }
-}
-
-class SolutionII {
-    // Same as Solution
-    public double findMaxAverage(int[] nums, int k) {
-        if (nums == null || nums.length < k || k < 0 || k > nums.length) return 0;
-
-        int sum = 0;
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
         int max = sum;
 
-        for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];
+        int l, r;
+        for (l = 0, r = k; r < nums.length; l++, r++) {
+            sum += nums[r] - nums[l];
             max = Math.max(max, sum);
         }
         return (double) max / k;

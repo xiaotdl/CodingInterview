8a9e2c61b70fc57308fcaf64bb25b809f0d6969b
xiaotdl <xiaotdl@gmail.com>
2017-09-26 23:12:16 -0700

    update

--- LeetCode/java/src/find_the_duplicate_number/Solution.java
+++ LeetCode/java/src/find_the_duplicate_number/Solution.java
@@ -4,49 +4,46 @@ package find_the_duplicate_number;
  * Created by Xiaotian on 11/1/16.
  */
 public class Solution {
+    // credit: https://discuss.leetcode.com/topic/25580/two-solutions-with-explanation-o-nlog-n-and-o-n-time-o-1-space-without-changing-the-input-array
     // tag: binary search
     // time: O(nlogn), find duplicate in logn times, each time loop through n nums
     // space: O(1)
+    /*
+     * @param nums: an array containing n + 1 integers which is between 1 and n
+     * @return: the duplicate one
+     */
     public int findDuplicate(int[] nums) {
-        if (nums == null) {
-            return -1;
-        }
-
-        int start = 1;
-        int end = nums.length;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            int cntLessThanMid = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] < mid) {
-                    cntLessThanMid++;
-                }
-            }
-            if (cntLessThanMid < mid) {
-                // duplicate resides in rhs
-                start = mid;
-            }
-            else {
-                // duplicate resides in lhs
-                end = mid;
+        int l = 1;
+        int r = nums.length - 1;
+        while (l + 1 < r) {
+            int m = l + (r - l) / 2;
+            if (smallerNumsCnt(nums, m) < m) {
+                l = m;
+            } else {
+                r = m;
             }
         }
-
-        if (isDuplicate(start, nums)) {
-            return start;
-        }
-
-        if (isDuplicate(end, nums)) {
-            return end;
-        }
-
+        if (isDuplicate(nums, l)) return l;
+        if (isDuplicate(nums, r)) return r;
         return -1;
     }
 
-    boolean isDuplicate(int num, int[] nums) {
-        for (int currNum : nums) {
-            if (currNum == num) {
-                return true;
+    private int smallerNumsCnt(int[] nums, int target) {
+        int cnt = 0;
+        for (int num : nums) {
+            if (num < target) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    private boolean isDuplicate(int[] nums, int target) {
+        int cnt = 0;
+        for (int num : nums) {
+            if (num == target) {
+                cnt++;
+                if (cnt == 2) return true;
             }
         }
         return false;

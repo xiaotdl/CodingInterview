649ee63f7aca88ff2583145f39b5d11695154ed0
xiaotdl <xiaotdl@gmail.com>
2017-10-13 01:20:53 -0700

    init

--- LeetCode/java/src/valid_number/Solution.java
+++ LeetCode/java/src/valid_number/Solution.java
@@ -43,4 +43,67 @@ public class Solution {
     }
 }
 
+class SolutionII {
+    // regex
+    // int: \s+[+-]?\d+(\.)?\s+
+    // float: \s+[+-]?(?:\d+|\d*\.\d+)\s+
+    public boolean isNumber(String s) {
+        if (s == null || s.length() == 0 || s.trim().length() == 0) return false;
+
+//        return s.matches("^\\s*[-+]?(?:[0-9]+(\\.[0-9]*)?|\\.[0-9]+)(e[-+]?[0-9]+)?\\s*$");
+        //                                 int     |    float        scientific
+        return s.matches("^\\s*[-+]?(?:[0-9]+(\\.)?|[0-9]*\\.[0-9]+)(e[-+]?[0-9]+)?\\s*$");
+    }
+}
+
+class SolutionIII {
+    // Deterministic Finite Automaton
+
+    // Ref:
+    // https://www.tutorialspoint.com/automata_theory/deterministic_finite_automaton.htm
+    // Formal Definition of a DFA
+    // A DFA can be represented by a 5-tuple (Q, ∑, δ, q0, F) where −
+    //   - Q is a [finite set of states].
+    //   - ∑ is a finite set of symbols called the [alphabet].
+    //   - δ is the [transition function] where δ: Q × ∑ → Q
+    //   - q0 is the [initial state] from where any input is processed (q0 ∈ Q).
+    //   - F is a set of [final state/states] of Q (F ⊆ Q).
+
+    // Ref: convert regex to nfa/dfa
+    // http://hackingoff.com/compilers/regular-expression-to-nfa-dfa
+    // \s*[-+]?(\d+(\.)?|\d*\.\d+)(e[-+]?\d+)?\s*
+
+    // Ref: test cases
+    // https://discuss.leetcode.com/topic/1095/the-worst-problem-i-have-ever-met-in-this-oj/11
+    // == common ==
+    // prefix/suffix spaces are allowed
+    // leading zeroes are allowed
+
+    // == int ==
+    // "0" => true
+
+    // == float ==
+    // " 0.1 " => true
+    // "0." => true
+
+    // == scientific == (eX == 10^X)
+    // "2e10" => true
+    // "0.5e-10" => true
+    // "3e" => false
+    // "e9" => false
+
+    // == invalid ==
+    // null => false
+    // "" => false
+    // "   " => false
+    // "1  1" => false
+    // "abc" => false
+    // "1 a" => false
+
+    // Solution:
+    // https://discuss.leetcode.com/topic/30058/a-simple-solution-in-python-based-on-dfa
+}
+
+
+
 

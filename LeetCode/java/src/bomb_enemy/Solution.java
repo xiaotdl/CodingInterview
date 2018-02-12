2acbff4f444a50183c50314fbb9d0418054a1bad
xiaotdl <xiaotdl@gmail.com>
2018-02-12 02:22:14 -0800

    update

--- LeetCode/java/src/bomb_enemy/Solution.java
+++ LeetCode/java/src/bomb_enemy/Solution.java
@@ -3,21 +3,24 @@ package bomb_enemy;
 /**
  * Created by Xiaotian on 12/26/16.
  */
-// tag: dp
-// time: O(mn)
-// space: O(m+n), can be optimized to n using int rowKills
 public class Solution {
+    // Even though it looks like having 3 for loops, one last for loop will not be counted
+    // because we only count at the point where the wall starts, so every cell will be visited at most some constant times.
+    // (2 or 3 whatever, depending on how we define ‘visit cell’.)
+    // tag: dp, dfs
+    // time: O(mn)
+    // space: O(m+n), can be optimized to n using int rowKills
     public int maxKilledEnemies(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-        int maxKills = 0;
         int m = grid.length;
         int n = grid[0].length;
-        int[] rowKills = new int[m];
-        int[] colKills = new int[n];
+        int[] rowKills = new int[m]; // rowKills[i]: enemies killed in a row
+        int[] colKills = new int[n]; // colKills[j]: enemies killed in a col
+        int maxKills = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || grid[i - 1][j] == 'W') {
-                    colKills[j] = 0;
+                    colKills[j] = 0;  // cnt needs to be refreshed when there is a new wall
                     for (int k = i; k < m && grid[k][j] != 'W'; k++) {
                         colKills[j] += grid[k][j] == 'E' ? 1 : 0;
                     }

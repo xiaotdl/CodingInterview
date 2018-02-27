485a7a50a08d4dae4b5718f4c7696931e95704d1
xiaotdl <xiaotdl@gmail.com>
2018-02-26 23:55:43 -0800

    init

--- LeetCode/java/src/first_position_unique_character/Solution.java
+++ LeetCode/java/src/first_position_unique_character/Solution.java
@@ -1,9 +1,12 @@
 package first_position_unique_character;
 
+import java.util.*;
+
 /**
  * Created by Xiaotian on 1/25/18.
  */
 public class Solution {
+    // two pass
     // tag: hash
     // time: O(n)
     // space: O(1)
@@ -25,5 +28,35 @@ public class Solution {
         }
 
         return -1;
+    }
+}
+
+class SolutionII {
+    // one pass
+    // tag: hash
+    // time: O(n)
+    // space: O(1)
+    public int firstUniqChar(String s) {
+        char[] S = s.toCharArray();
+        int[] charIdx = new int[256]; // -1: init, -2: repeated
+        Arrays.fill(charIdx, -1);
+        for (int i = 0; i < s.length(); i++) {
+            if (charIdx[S[i]] == -1) { // first time
+                charIdx[S[i]] = i;
+            }
+            else if (charIdx[S[i]] == -2) {
+                continue;
+            }
+            else { // charIdx[S[i]] >= 0
+                charIdx[S[i]] = -2;
+            }
+        }
+        int minIdx = Integer.MAX_VALUE;
+        for (int i = 0; i < 256; i++) {
+            if (charIdx[i] >= 0) {
+                minIdx = Math.min(minIdx, charIdx[i]);
+            }
+        }
+        return minIdx != Integer.MAX_VALUE ? minIdx : - 1;
     }
 }
--- LeetCode/java/src/pow_x_n/Solution.java
+++ LeetCode/java/src/pow_x_n/Solution.java
@@ -4,7 +4,7 @@ package pow_x_n;
  * Created by Xiaotian on 9/25/17.
  */
 public class Solution {
-    // Note negative part
+    // Note when n is negative part
     // tag: math, divide and conquer
     // time: O(logn)
     // space: O(1)
@@ -19,6 +19,31 @@ public class Solution {
             return half * half * x;
         } else {
             return half * half / x;
+        }
+    }
+}
+
+class SolutionII {
+    // n < 0 & n >= 0
+    // tag: math, divide and conquer
+    // time: O(logn)
+    // space: O(1)
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return fastPow(x, n);
+    }
+
+    double fastPow(double x, int n) {
+        if (n == 0) return 1.0;
+
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
         }
     }
 }

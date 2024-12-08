94bb4fa47425af73dbd86b7b4e2ef070faee51c1
Xiaotian Li <xiaotdl@gmail.com>
2024-12-07 22:06:54 -0800

    Update 0003_Longest_Substring_Without_Repeating_Characters.cpp

--- LeetCode/cpp/0003_Longest_Substring_Without_Repeating_Characters.cpp
+++ LeetCode/cpp/0003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -4,18 +4,43 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        size_t longestSubstringSize = 0;
+        size_t maxLen = 0;
         // enumerate all substrings
         for (int i = 0; i < s.size(); ++i) { // start index of the substring
             set<char> seen;
             for (int j = i; j < s.size(); ++j) { // end index of the substring
                 if (seen.contains(s[j])) {
-                    longestSubstringSize = std::max(longestSubstringSize, seen.size());
+                    maxLen = std::max(maxLen, seen.size());
                     break;
                 }
                 seen.emplace(s[j]);
             }
         }
-        return longestSubstringSize;
+        return maxLen;
+    }
+};
+
+
+// tag: str, hash table, sliding window
+// time: O(n), at most 2n steps for i and j
+// space: O(1)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        size_t maxLen = 0;
+        set<char> seen;
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            while (j < s.size()) {
+                if (seen.contains(s[j])) {
+                    maxLen = std::max(maxLen, seen.size());
+                    break;
+                } else {
+                    seen.emplace(s[j]);
+                    ++j;
+                }
+            }
+            seen.erase(s[i]);
+        }
+        return maxLen;
     }
 };

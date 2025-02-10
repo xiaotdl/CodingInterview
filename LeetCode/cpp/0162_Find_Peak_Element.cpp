53d6a1fd241aa48dfc2d6caac1177982423cd1e6
Xiaotian Li <xiaotdl@gmail.com>
2025-02-10 14:56:34 -0800

    Create 0162_Find_Peak_Element.cpp

---
+++ LeetCode/cpp/0162_Find_Peak_Element.cpp
@@ -0,0 +1,14 @@
+// tag: brutal force
+// time: O(n)
+// space: O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if ((i == 0 || nums[i - 1] < nums[i]) && (i == nums.size() - 1 || nums[i] > nums[i + 1])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
